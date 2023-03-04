8b5c6b71300bca2105bf217f750f6f0a3dd72b6a
Amardeep Mann <amar2slick@gmail.com>
2023-03-03 17:37:49 -0800

    Peer Review and Comments

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+// Amardeep Mann
+// 3-3-23
+// Jobs that interest you
+// We created a single page React website about companies that we'd be interested in working at
+// Peer Review by Ken: "Spartan, but easy to navigate and easy on the eyes. No real bugs that I could find. The code was easy to follow. The company logos were a nice touch, along with the links to the career pages. Thoughtful and introspective assessments of the five companies as well. As always, good job!" -Kenneth Fujimura
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
--- src/components/Parsec.js
+++ src/components/Parsec.js
@@ -11,14 +11,14 @@ export default function Parsec() {
             <Container fluid className="pages-cont">
                 <Row className="gx-35">
                     <Col xs={3}>
-                        <img src='https://uploads-ssl.webflow.com/610fa4009677867f18e4b375/61fc3a33a32d0acc886ae1a1_backbone-icon.png' alt='Ebbo logo' width='175'/>
+                        <img src='https://i0.wp.com/articles.parsec.app/wp-content/uploads/2019/09/Parsec-Horizontal-Logo-Light-Background.png?ssl=1' alt='Ebbo logo' width='175'/>
                         <br/>
-                        <a href="https://jobs.lever.co/backbone">Careers Page</a>
+                        <a href="https://parsec.app/careers">Careers Page</a>
                     </Col>
                     <Col xs={9}>
-                        <p>Parsec is a cloud gaming perhiperal company that specializes in gaming controller accessories for mobile phones. It's rather small at the momement but in a position to grow rapidly as cloud gamin starts to become more and more viable. My goal would be to get in on the bottom floor to a tech sector that I think would be greatly in demand in the future.</p>
-                        <p>Their seems to a diverse number or software developer positions at Backbone. They have mobile developer jobs fro both ANdroid and iOS as well as back-end positions. Oddly enough the back-end position seems like one that I'd be most suited for out of Codestack. They're looking for someone with a strong proficiency in Javascript, Typescript, and Node.js who has an understanding of asynchronous programming. Those are all concepts we'll be introduced to at the very least while at Codestack.</p>
-                        <p>The positions are remote friendly but benefits don't seem to be outlined as concretely as I've seen with other companies. That may be a bit of a red flag. Also I've only seen listings for senior level engineers but I feel it'd be worthwhile to reach out and ask them about and junior level positions or internships they may be offering.</p>
+                        <p>Right off the bat: "Parsec is now a part of Unity". Amazing! Who would have thought all those months of grinding Unity during the combine would actually end up being useful work experience. Parsec itself ins't really a gaming company but rather one of the biggest players in the game streaming or cloud-gaming space. They are one of the most widely used for a reason. Their low-latency technology is considered to be the best available at the moment.</p>
+                        <p>My goal would be to get my foot in the door into a tech space that I see rapidly growing in the next few years. They employ a lot of front and back end technologies into their application which would also give me plenty of room to grow as a developer. </p>
+                        <p>On the front end side they use Javascript and React which we've covered in class. The core of their program is written in C though. Since we learned C# it shouldn't be too hard to get started in the learning process but I imagine it will take a significant learning investment to get adept enough to the point where I can consider applying.</p>
                     </Col>
                 </Row>
             </Container>
